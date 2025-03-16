8ec36c7d549499991b413e34ee33fe0c76a711ef
MFarisA <muhfarisassami@gmail.com>
2025-03-16 19:26:38 +0700

    Add user and config data tables to respective pages; implement dynamic breadcrumb navigation

--- app/configs/page.tsx
+++ app/configs/page.tsx
@@ -1,10 +1,16 @@
 'use client'
+import { ConfigDataTable } from "@/components/config-table";
 import React from "react";
 
-export default function configPage () {
+export default function configPage() {
     return (
-        <div>
-            <p>halo</p>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="aspect-video rounded-xl bg-blue-100" />
+                <div className="aspect-video rounded-xl bg-blue-100" />
+                <div className="aspect-video rounded-xl bg-blue-100" />
+            </div>
+            <ConfigDataTable />
         </div>
     )
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,16 @@
 'use client'
+import { UserDataTable } from "@/components/user-table";
 import React from "react";
 
-export default function UsersPage () {
+export default function UsersPage() {
     return (
-        <div>
-            <p>halo</p>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="aspect-video rounded-xl bg-blue-100" />
+                <div className="aspect-video rounded-xl bg-blue-100" />
+                <div className="aspect-video rounded-xl bg-blue-100" />
+            </div>
+            <UserDataTable />
         </div>
     )
 }
